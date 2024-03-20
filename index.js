2403111c685be7c51b68794483575ea2c4391725
Tracy Karanja <chirutracy@gmail.com>
2024-03-20 14:59:17 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(b = "*"){
+    const day = function(x ='special'){
+        return `You are ${b}${x}${b}!`
+    }
+    return day;
+}
+console.log(wrapAdjective("%")("a dedicated programmer"));
